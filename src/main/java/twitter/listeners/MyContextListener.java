8c43462fe30b70f2ce479e80246720469e8a48f7
Dmytro Deinichenko <Dmytro_Deinichenko@epam.com>
2017-04-18 14:34:40 +0300

    Listener refactored.

--- src/main/java/twitter/listeners/MyContextListener.java
+++ src/main/java/twitter/listeners/MyContextListener.java
@@ -11,11 +11,20 @@ public class MyContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        System.out.println("MyContextListener: context initialized...");
+        /*System.out.println("MyContextListener: context initialized...");
         String [] rootContextFileNames = servletContextEvent
                 .getServletContext()
                 .getInitParameter("contextConfigLocation").split(" ");
         ConfigurableApplicationContext rootContext = new ClassPathXmlApplicationContext(rootContextFileNames);
+        servletContextEvent.getServletContext().setAttribute("context", rootContext);*/
+
+        String [] rootContextFileNames = servletContextEvent
+                .getServletContext()
+                .getInitParameter("contextConfigLocation").split(" ");
+        ConfigurableApplicationContext rootContext = null;
+        for (int i = 0; i < rootContextFileNames.length; i++) {
+            rootContext = new ClassPathXmlApplicationContext(new String[]{rootContextFileNames[i]}, rootContext);
+        }
         servletContextEvent.getServletContext().setAttribute("context", rootContext);
     }
 
